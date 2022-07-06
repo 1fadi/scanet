d3dcd8b5e2059daf8fefa3a9e87b7737fa11261b
Fadi <95546528+1fadi@users.noreply.github.com>
2022-07-06 03:33:24 +0200

    edited subclass, added params

--- portscanner.py
+++ portscanner.py
@@ -12,46 +12,49 @@ RED = Fore.RED
 ascii_banner = figlet_format("Port scanner")
 print(f"{GREEN}{str(ascii_banner)}{RESET}")
 
-# TARGET IP
-IP = "192.168.178.1"
-# IP = "localhost"
+# TARGET
+IP = input("\nEnter target IP: ")
 port_range = range(1, 62115)  # or create a list instead
+running_threads = int(input("Enter number of threads to run: "))
 
 queue = Queue()
 
 
 class Scanner(threading.Thread):
-    def __int__(self):
+    def __init__(self, ip, _queue):
         threading.Thread.__init__(self)
-        self.IP = IP
+        self.ip = ip
+        self._queue = _queue
 
     def run(self):
+        global GREEN
+        global RESET
         """
         it keeps looping as long as there are ports available to scan
         """
-        global queue
-        global IP
-        while not queue.empty():
-            port = queue.get()
+        while not self._queue.empty():
+            port = self._queue.get()
             try:
                 # if a connection was successful, the port will be printed.
                 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                sock.connect((IP, port))
-                print(f"{GREEN} [+]{RESET} {IP}:{port}")
+                sock.connect((self.ip, port))
+                print(f"{GREEN} [+]{RESET} {self.ip}:{port}")
             except:
                 # ports that are not open are skipped.
                 continue
 
 
-def fill_queue(port_range):
-    for port in port_range:
-        queue.put(port)
+def fill_queue(_port_range, _queue):
+    for port in _port_range:
+        _queue.put(port)
 
 
-def manager(n):  # Set how many threads to run.
+def manager(_range, ip, _queue):
+    global RED
+    global RESET
     threads = []
-    for i in range(n):
-        thread = Scanner()
+    for i in range(_range):  # Set how many threads to run.
+        thread = Scanner(ip, _queue)
         threads.append(thread)
 
     for thread in threads:
@@ -64,7 +67,8 @@ def manager(n):  # Set how many threads to run.
         print(f"{RED}exiting..{RESET}")
 
 
-fill_queue(port_range)
-manager(100)
+if __name__ == "__main__":
+    fill_queue(port_range, queue)
+    manager(running_threads, IP, queue)
 
-print("\nscanning finished.\n")
+    print("\nscanning finished.\n")
